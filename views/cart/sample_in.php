6cb8fb0b1c85222dbe5e9d4a88bd9a3fe4111c09
Gregory Popov <greg.georg.popov@gmail.com>
2016-09-20 16:42:54 +0300

    Cart fix

--- views/cart/sample_in.php
+++ views/cart/sample_in.php
@@ -7,5 +7,5 @@
     </td>
     <td data-title="Product" class="product-name"><a href="<?= _A_::$app->router()->UrlTo('product',['p_id'=>$p_id,'back'=>'cart']); ?>"><?= $item['Product_name']; ?></a></td>
     <td data-title="Quantity" class="product-quantity"><span class="quantity"><?= $item['quantity']; ?></span></td>
-    <td class="product-remove"><a id="del_sample_cart" href="<?= _A_::$app->router()->UrlTo('cart/del_sample');?>"><i class=" fa fa-trash-o"></i></a></td>
+    <td class="product-remove text-right"><a id="del_sample_cart" href="<?= _A_::$app->router()->UrlTo('cart/del_sample');?>"><i class=" fa fa-trash-o"></i></a></td>
 </tr>
