d881b0f276ef1cedc06af29c779b2df37406772d
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-09-20 19:05:09 +0300

    router sef url

--- classes/router.php
+++ classes/router.php
@@ -12,6 +12,8 @@ Class Router
 
     private $path;
 
+    private $exclude_params = ['page', 'back'];
+
     public function start()
     {
         $this->init();
@@ -35,6 +37,80 @@ Class Router
         } catch (Exception $e) {
             (new Controller_Main())->error404($e->getMessage());
         }
+    }
+
+    protected function init()
+    {
+        if (!function_exists('http_build_url')) {
+            define('HTTP_URL_REPLACE', 1);                // Replace every part of the first URL when there's one of the second URL
+            define('HTTP_URL_JOIN_PATH', 2);            // Join relative paths
+            define('HTTP_URL_JOIN_QUERY', 4);            // Join query strings
+            define('HTTP_URL_STRIP_USER', 8);            // Strip any user authentication information
+            define('HTTP_URL_STRIP_PASS', 16);            // Strip any password authentication information
+            define('HTTP_URL_STRIP_AUTH', 32);            // Strip any authentication information
+            define('HTTP_URL_STRIP_PORT', 64);            // Strip explicit port numbers
+            define('HTTP_URL_STRIP_PATH', 128);            // Strip complete path
+            define('HTTP_URL_STRIP_QUERY', 256);        // Strip query string
+            define('HTTP_URL_STRIP_FRAGMENT', 512);        // Strip any fragments (#identifier)
+            define('HTTP_URL_STRIP_ALL', 1024);            // Strip anything but scheme and host
+        }
+
+        $this->parse_url();
+        $this->route = (empty(_A_::$app->get('route'))) ? '' : _A_::$app->get('route');
+        if (empty($this->route)) $this->route = 'index';
+        $this->route = trim($this->route, '/\\');
+        $this->setPath(SITE_PATH . 'controllers' . DS);
+
+    }
+
+    private function parse_url()
+    {
+        $query_string = _A_::$app->server('QUERY_STRING');
+        $request_uri = _A_::$app->server('REQUEST_URI');
+        parse_str($query_string, $query);
+        $query_sef_url = $query['route'];
+        $query_path = str_replace('?', '&', $this->revert_sef_url($query_sef_url));
+        $query_string = str_replace($query_sef_url, $query_path, $query_string);
+        $request_uri = str_replace($query_sef_url, $query_path, $request_uri);
+        parse_str($query_string, $query);
+        _A_::$app->server('QUERY_STRING', $query_string);
+        _A_::$app->server('REQUEST_URI', $request_uri);
+        _A_::$app->setGet($query);
+    }
+
+    private function revert_sef_url($sef_url, $suff = null, $pref = null)
+    {
+        $url = $sef_url;
+        if (strlen(trim($pref))) {
+            $pref = $this->convert($pref);
+            if (strlen(trim($pref))) $sef_url = str_replace(trim($pref), '', $sef_url);
+        }
+        if (strlen(trim($suff))) {
+            $suff = $this->convert($suff);
+            if (strlen(trim($suff))) $sef_url = str_replace(trim($suff), '', $sef_url);
+        }
+        $sef_url = preg_replace('/-$/i', '', preg_replace('/^-/i', '', $sef_url));
+        $url = Model_Router::get_url($sef_url, $url);
+        return $url;
+    }
+
+    private function convert($in)
+    {
+        $out = preg_replace('/[^a-zA-Z0-9]+/i', ' ', $in);
+        $out = trim(preg_replace('/\s{2,}/', ' ', $out));
+        $out = str_replace(' ', '-', $out);
+        return strtolower($out);
+    }
+
+    private function setPath($path)
+    {
+        $path = rtrim($path, '/\\');
+        $path .= DS;
+
+        if (is_dir($path) == false) {
+            throw new Exception ('Invalid controller path: `' . $path . '`');
+        }
+        $this->path = $path;
     }
 
     function getController()
@@ -61,15 +137,15 @@ Class Router
 
     private function setBaseUrl()
     {
-        $end_uri = explode('/', _A_::$app->server('REQUEST_URI'));
-        $end_get = explode('/', _A_::$app->get('route'));
+        $end_uri = explode(DS, _A_::$app->server('REQUEST_URI'));
+        $end_get = explode(DS, _A_::$app->get('route'));
         array_pop($end_uri);
         $end_uri = array_filter(array_diff($end_uri, $end_get));
         if ($this->action == 'post') array_pop($end_uri);
-        $this->base_url = strtolower(explode('/', _A_::$app->server('SERVER_PROTOCOL'))[0]) .
+        $this->base_url = strtolower(explode(DS, _A_::$app->server('SERVER_PROTOCOL'))[0]) .
             "://" . _A_::$app->server('SERVER_NAME') .
             (_A_::$app->server('SERVER_PORT') == '80' ? '' : ':' . _A_::$app->server('SERVER_PORT'));
-        if(count($end_uri)) $this->base_url .= DS . implode('/', $end_uri);
+        if (count($end_uri)) $this->base_url .= DS . implode(DS, $end_uri);
         define('BASE_URL', $this->base_url);
     }
 
@@ -84,17 +160,30 @@ Class Router
         return $url;
     }
 
-    public function UrlTo($path, $params = null)
+    public function UrlTo($path, $params = null, $to_sef = null, $sef_exclude_params = [])
     {
+        $sef_exclude_params = array_merge($this->exclude_params, $sef_exclude_params);
         $path = rtrim(trim($path), DS);
         if (strpos($path, '{base_url}') !== false) {
             $path = str_replace('{base_url}', $this->base_url, $path);
-        } else {
-            if (strpos($path, $this->base_url) == false)
-                $path = $this->base_url . DS . $path;
         }
-
-        if (!is_null($params)) $url = $this->http_build_url($path, ['query'=>http_build_query($params)]);
+        $_path = $path;
+        if (strpos($_path, $this->base_url) !== false) $_path = trim(str_replace($this->base_url, '', $path), '/\\');
+        if (preg_match('#(.*)\?(.*)#i', $_path, $matches)) $_path = $matches[1];
+        if (count($matches) > 2) {
+            parse_str($matches[2], $_params);
+            $params = array_merge($params, $_params);
+        }
+        $sef_include_params = isset($params) ? array_diff_key($params, array_flip($sef_exclude_params)) : [];
+        $_path = $this->http_build_url(trim($_path, DS), ['query' => http_build_query($sef_include_params)]);
+        if (isset($to_sef)) {
+            $path = $this->build_sef_url($to_sef, $_path);
+        } else {
+            $path = Model_Router::get_sef_url($_path);
+        }
+        $params = isset($params) ? array_intersect_key($params, array_flip($sef_exclude_params)) : [];
+        if (strpos($path, $this->base_url) == false) $path = $this->base_url . DS . $path;
+        if (!is_null($params) && is_array($params) && (count($params) > 0)) $url = $this->http_build_url($path, ['query' => http_build_query($params)]);
         else $url = $this->http_build_url($path);
         return $url;
     }
@@ -177,37 +266,19 @@ Class Router
 
     }
 
-    protected function init()
+    private function build_sef_url($to_url, $url, $suff = null, $pref = null)
     {
-        if (!function_exists('http_build_url')) {
-            define('HTTP_URL_REPLACE', 1);                // Replace every part of the first URL when there's one of the second URL
-            define('HTTP_URL_JOIN_PATH', 2);            // Join relative paths
-            define('HTTP_URL_JOIN_QUERY', 4);            // Join query strings
-            define('HTTP_URL_STRIP_USER', 8);            // Strip any user authentication information
-            define('HTTP_URL_STRIP_PASS', 16);            // Strip any password authentication information
-            define('HTTP_URL_STRIP_AUTH', 32);            // Strip any authentication information
-            define('HTTP_URL_STRIP_PORT', 64);            // Strip explicit port numbers
-            define('HTTP_URL_STRIP_PATH', 128);            // Strip complete path
-            define('HTTP_URL_STRIP_QUERY', 256);        // Strip query string
-            define('HTTP_URL_STRIP_FRAGMENT', 512);        // Strip any fragments (#identifier)
-            define('HTTP_URL_STRIP_ALL', 1024);            // Strip anything but scheme and host
+        $sef_url = $this->convert(trim($to_url));
+        $url = Model_Router::set_sef_url($sef_url, $url);
+        if (strlen(trim($pref))) {
+            $pref = $this->convert($pref);
+            if (strlen(trim($pref))) $url = $pref . '-' . trim($url);
         }
-
-        $this->route = (empty(_A_::$app->get('route'))) ? '' : _A_::$app->get('route');
-        if (empty($this->route)) $this->route = 'index';
-        $this->route = trim($this->route, '/\\');
-        $this->setPath(SITE_PATH . 'controllers' . DS);
-
-    }
-
-    private function setPath($path)
-    {
-        $path = rtrim($path, '/\\');
-        $path .= DS;
-
-        if (is_dir($path) == false) {
-            throw new Exception ('Invalid controller path: `' . $path . '`');
+        if (strlen(trim($suff))) {
+            $suff = $this->convert($suff);
+            if (strlen(trim($suff))) $url = $url . '-' . trim($suff);
         }
-        $this->path = $path;
+        $url = strtolower($url);
+        return $url;
     }
 }
---
+++ models/model_router.php
@@ -0,0 +1,55 @@
+<?php
+
+Class Model_Router extends Model_Model
+{
+
+    public static function set_sef_url($sef_url, $url)
+    {
+        $_sef_url = $sef_url;
+        $itherator = 0;
+        while (true) {
+            $sql = "SELECT * from url_sef where sef = '" . mysql_escape_string($sef_url) . "'";
+            $find_result = mysql_query($sql);
+            if (!mysql_num_rows($find_result)) {
+                $sql = "INSERT INTO url_sef(url,sef) VALUES('" . $url . "', '" . mysql_escape_string($sef_url) . "')";
+                mysql_query($sql);
+                break;
+            } else {
+                $res = mysql_fetch_assoc($find_result);
+                if ($res['url'] !== $url) {
+                    $itherator += 1;
+                    $sef_url = $_sef_url . '-' . $itherator;
+                } else break;
+            }
+        }
+        return $sef_url;
+    }
+
+    public static function get_sef_url($url)
+    {
+        $sef_url = $url;
+        $sql = "SELECT * from url_sef where url = '" . mysql_escape_string($url) . "'";
+        $find_result = mysql_query($sql);
+        if ($find_result && mysql_num_rows($find_result)) {
+            $res = mysql_fetch_assoc($find_result);
+            $sef_url = $res['sef'];
+        }
+        return $sef_url;
+    }
+
+    public
+    static function get_url($sef_url)
+    {
+        $url = $sef_url;
+        if ($sef_url != '') {
+            $sql = 'SELECT * FROM url_sef WHERE sef = "' . mysql_escape_string($sef_url) . '"';
+            $find_result = mysql_query($sql);
+            if (mysql_num_rows($find_result)) {
+                $res = mysql_fetch_assoc($find_result);
+                $url = $res['url'];
+            }
+        }
+        return $url;
+    }
+
+}
