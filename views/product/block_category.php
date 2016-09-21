b87b7f0de9ef7b6570171b340147345dde3519ef
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-09-21 15:04:48 +0300

    product category

---
+++ views/product/block_category.php
@@ -0,0 +1,6 @@
+<li class="category">
+    <div class="category_order">
+        <label><?= $category[1] ?></label>
+        <input name="category[<?= $cat_id ?>]" value="<?= $category[0] ?>">
+    </div>
+</li>
