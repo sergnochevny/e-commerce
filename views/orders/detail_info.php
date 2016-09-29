beb971837e9a0cb69d206d32ad8443eac02460ea
Gregory Popov <greg.georg.popov@gmail.com>
2016-09-29 14:32:45 +0300

    UI fix

--- views/orders/detail_info.php
+++ views/orders/detail_info.php
@@ -1,5 +1,5 @@
 <tr>
-    <td><?= $is_sample ? 'SAMPLE &mdash; '.$product_name : $product_name; ?></td>
+    <td><span class="cut-text-in-one-line"><?= $is_sample ? 'SAMPLE &mdash; '.$product_name : $product_name; ?></span></td>
     <td></td>
     <td><?= $sale_price ?></td>
     <td><?= $quantity ?></td>
