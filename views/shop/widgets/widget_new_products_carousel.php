be221fe3e38e790731b45f6fea3879032da9e84c
Vadim <vadym.aminov@engineering.ait.com>
2016-10-05 17:17:07 +0300

    scale carousel block img

--- views/shop/widgets/widget_new_products_carousel.php
+++ views/shop/widgets/widget_new_products_carousel.php
@@ -2,7 +2,8 @@
 echo $last ? 'last' : ''; ?> product type-product status-publish has-post-thumbnail product_cat-brooches product_tag-fashion product_tag-jewelry sale featured shipping-taxable purchasable product-type-simple product-cat-brooches product-tag-fashion product-tag-jewelry instock">
     <div class="product-inner">
         <a href="<?= _A_::$app->router()->UrlTo('product',['p_id'=>$row[0]], $row['pname']); ?>">
-            <figure class="product-image-box" style="background-image:url(<?= $filename; ?>)">
+            <figure class="product-image-box">
+                <img src="<?= $filename; ?>" alt="">
                 <?php if ($bProductDiscount) { ?>
                     <span class="extra_discount">Extra Discount!</span>
                 <?php } ?>
