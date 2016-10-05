df30dee0a79de093453af46d89e5bcbde111ccb0
Vadim <vadym.aminov@engineering.ait.com>
2016-10-05 16:05:15 +0300

    hover effect in admin blog page

--- views/blog/admin_posts.php
+++ views/blog/admin_posts.php
@@ -1,11 +1,16 @@
 <div class="just-post col-xs-12 col-sm-6 col-md-4">
-    <div style="background-image: url('<?= $post_img; ?>');" class="just-post-image just-post-image-admin">
-        <a id="del_post"
-           href="<?= $post_del_href; ?>" rel="nofollow"
-           data-post_id="<?= $post_id ; ?>" data-product_sku="" data-quantity="1"
-           class="button icon-delete add_to_cart_button   post_type_simple"></a>
-        <a id="edit_post" href="<?= $post_edit_href; ?>"
-           class="button product-button icon-modify"></a>
+    <div class="just-post-image just-post-image-admin">
+        <figure>
+            <img src="<?= $post_img; ?>" alt="">
+            <figcaption>
+                <a id="del_post"
+                   href="<?= $post_del_href; ?>" rel="nofollow"
+                   data-post_id="<?= $post_id ; ?>" data-product_sku="" data-quantity="1"
+                   class="admin-button button icon-delete add_to_cart_button post_type_simple"></a>
+                <a id="edit_post" href="<?= $post_edit_href; ?>"
+                   class="admin-button button product-button icon-modify"></a>
+            </figcaption>
+        </figure>
     </div>
     <div class="just-post-detail">
         <h3 class="post-title"><a href="<?= $post_edit_href; ?>"><?= isset($post_title) ? $post_title : ''; ?></a></h3>
--- views/blog/posts.php
+++ views/blog/posts.php
@@ -1,5 +1,5 @@
 <div class="just-post col-xs-12 col-sm-6 col-md-4">
-    <div class="just-post-image" style="background-image: url('<?= isset($post_img)?$post_img:'';?>');"><a href="<?= $post_href;?>"></a></div>
+    <div class="just-post-image" style="background-image: url('<?= isset($post_img)?$post_img:'';?>');"><a class="hover-button" href="<?= $post_href;?>"></a></div>
     <div class="just-post-detail">
         <h3 class="post-title"><a href="<?= $post_href;?>"><?= isset($post_title)?$post_title:'';?></a></h3>
         <div class="just-divider text-center line-yes icon-hide">
