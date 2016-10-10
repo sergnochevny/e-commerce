c1e6a30ce559b1ab200422d577d2a5317969f72d
Gregory Popov <greg.georg.popov@gmail.com>
2016-10-10 16:39:48 +0300

    Filter forms added 3

--- views/users/list.php
+++ views/users/list.php
@@ -26,6 +26,7 @@
   </a>
 </div>
 <p class="woocommerce-result-count">Showing <?= $count_rows; ?> results</p>
+<?php include "views/users/filter_form.php"; ?>
 <div class="">
   <?= $list; ?>
 </div>
