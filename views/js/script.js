79b36517f4f641e3c48c8fde8b1b0bbfba1751b9
Gregory Popov <greg.georg.popov@gmail.com>
2016-10-10 12:08:06 +0300

    Fade animation added

--- views/js/script.js
+++ views/js/script.js
@@ -1,5 +1,37 @@
 'use strict';
+
+function addClass(elements, className) {
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (element.classList) {
+            element.classList.add(className);
+        } else {
+            element.className += ' ' + className;
+        }
+    }
+}
+
+// function welcome() {
+//     var el = document.getElementsByTagName('body');
+//     addClass([el[0]], 'loaded');
+// }
+// document.addEventListener("DOMContentLoaded", welcome);
+//
+
 jQuery(document).ready(function ($) {
+
+
+    // $(document).on("click", "a", function (e) {
+    //     e.preventDefault();
+    //     var linkClicked = e.target;
+    //     document.addEventListener("unload", $('body').removeClass('loaded').addClass('leaved'));
+    //     window.setTimeout(function () {
+    //         window.location = linkClicked.href;
+    //     }, 500);
+    //
+    //     return false;
+    // });
+
 
     function throwDanger(url, data, htmlData) {
         $.post(
