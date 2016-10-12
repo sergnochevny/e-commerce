efebd4354a4376b08395e7cabec776d6b1790fde
Gregory Popov <greg.georg.popov@gmail.com>
2016-10-12 15:49:10 +0300

    Categories remastered

--- views/categories/list.php
+++ views/categories/list.php
@@ -1,40 +1,55 @@
 <?php if(isset($warning)) { ?>
-  <div class="col-xs-12 alert-success danger" style="display: block;">
-    <?php
-      foreach($warning as $msg) {
-        echo $msg . "<br/>";
-      }
-    ?>
+  <div class="col-xs-12 alert-success danger">
+    <?php foreach($warning as $msg) { echo $msg . "<br/>"; } ?>
   </div>
-<?php } ?>
-<?php if(isset($error)) { ?>
-  <div class="col-xs-12 alert-danger danger" style="display: block;">
-    <?php
-      foreach($error as $msg) {
-        echo $msg . "<br/>";
-      }
-    ?>
+<?php } if(isset($error)) { ?>
+  <div class="col-xs-12 alert-danger danger">
+    <?php foreach($error as $msg) { echo $msg . "<br/>"; }?>
   </div>
-<?php } ?>
-<?php
+<?php }
   $prms = null;
   if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page');
 ?>
-<div class="text-center">
-  <a href="<?= _A_::$app->router()->UrlTo('categories/add',$prms); ?>" data-modify class="button">
-    ADD NEW CATEGORY
-  </a>
-</div>
-<p class="woocommerce-result-count">Showing <?= $count_rows; ?> results</p>
+
 <?= isset($search_form) ? $search_form : '' ?>
-<div>
-  <?= $list; ?>
+
+<div class="col-xs-12">
+  <div class="row">
+    <div class="col-xs-12 data-list-view panel panel-default">
+
+      <div class="col-xs-12 panel-header data-list-view-header relative">
+
+        <div class="col-xs-12 col-sm-8">
+          <div class="row xs-text-center">
+            <p class="woocommerce-result-count">Showing <?= $count_rows; ?> results</p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+          <div class="row text-right xs-text-center">
+            <a href="<?= _A_::$app->router()->UrlTo('categories/add',$prms); ?>" data-modify class="btn">
+              ADD NEW CATEGORY
+            </a>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="panel-body data-list-view-body">
+        <div class="row">
+          <?= $list; ?>
+        </div>
+      </div>
+      <div class="panel-footer">
+        <nav class="paging-navigation" role="navigation">
+          <h4 class="sr-only">Navigation</h4>
+          <ul class="pagination">
+            <?= isset($paginator) ? $paginator : ''; ?>
+          </ul>
+        </nav>
+      </div>
+
+    </div>
+  </div>
 </div>
-<nav class="paging-navigation" role="navigation">
-  <h4 class="sr-only">Navigation</h4>
-  <ul class="pagination">
-    <?= isset($paginator) ? $paginator : ''; ?>
-  </ul>
-</nav>
 
 <script src='<?= _A_::$app->router()->UrlTo('views/js/simple/list.js'); ?>' type="text/javascript"></script>
--- views/categories/rows.php
+++ views/categories/rows.php
@@ -1,27 +1,16 @@
-<table class="table table-striped table-bordered">
-  <thead>
-  <tr>
-    <th class="text-center">Name</th>
-    <th class="text-center">Display Order</th>
-    <th></th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php foreach($rows as $row): ?>
-    <?php
-    $prms['cid'] = $row[0];
-    if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page');
-    ?>
-    <tr>
-      <td>
-        <div class="text-center"><?= $row[1] ?></div>
-      </td>
-      <td>
-        <div class="text-center"><?= $row[2] ?></div>
-      </td>
-      <td>
-        <div class="text-center">
-          <figcaption>
+
+<?php foreach($rows as $row): $prms['cid'] = $row[0]; if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
+  <div class="col-xs-12 col-sm-6 data-list-view-item-container">
+    <div class="row">
+        <div class="col-xs-7 col-sm-6 xs-text-center">
+            <small class="data-hint">Category name</small><br>
+            <?= $row[1] ?>
+          </div>
+          <div class="col-xs-5 col-sm-5 xs-text-center">
+            <small class="data-hint">Display order</small><br>
+            <?= $row[2] ?>
+          </div>
+          <div class="col-xs-12 col-sm-1 xs-text-center xs-buttons-container buttons-container text-right">
             <a data-modify href="<?= _A_::$app->router()->UrlTo('categories/edit', $prms) ?>">
               <i class="fa fa-pencil"></i>
             </a>
@@ -29,11 +18,48 @@
                class="text-danger <?= ($row[2]>0)?'disabled':''?>">
               <i class=" fa fa-trash-o"></i>
             </a>
-          </figcaption>
-        </div>
-      </td>
-    </tr>
-  <?php endforeach; ?>
-  </tbody>
-</table>
+          </div>
+    </div>
+  </div>
+<?php endforeach; ?>
+
+<!---->
+<!--<table class="table table-striped table-bordered">-->
+<!--  <thead>-->
+<!--  <tr>-->
+<!--    <th class="text-center">Name</th>-->
+<!--    <th class="text-center">Display Order</th>-->
+<!--    <th></th>-->
+<!--  </tr>-->
+<!--  </thead>-->
+<!--  <tbody>-->
+<!--  --><?php //foreach($rows as $row): ?>
+<!--    --><?php
+//    $prms['cid'] = $row[0];
+//    if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page');
+//    ?>
+<!--    <tr>-->
+<!--      <td>-->
+<!--        <div class="text-center"></div>-->
+<!--      </td>-->
+<!--      <td>-->
+<!--        <div class="text-center"></div>-->
+<!--      </td>-->
+<!--      <td>-->
+<!--        <div class="text-center">-->
+<!--          <figcaption>-->
+<!--            <a data-modify href="--><?// _A_::$app->router()->UrlTo('categories/edit', $prms) ?><!--">-->
+<!--              <i class="fa fa-pencil"></i>-->
+<!--            </a>-->
+<!--            <a href="--><?// _A_::$app->router()->UrlTo('categories/delete', $prms) ?><!--" data-delete rel="nofollow"-->
+<!--               class="text-danger --><?// ($row[2]>0)?'disabled':''?><!--">-->
+<!--              <i class=" fa fa-trash-o"></i>-->
+<!--            </a>-->
+<!--          </figcaption>-->
+<!--        </div>-->
+<!--      </td>-->
+<!--    </tr>-->
+<!--  --><?php //endforeach; ?>
+<!--  </tbody>-->
+<!--</table>-->
 
