d411e65717fdd5090e94d42fcad785f0958dbb59
Gregory Popov <greg.georg.popov@gmail.com>
2016-10-21 16:42:45 +0300

    Added treatment for empty search result

--- views/categories/rows.php
+++ views/categories/rows.php
@@ -1,43 +1,49 @@
-<div class="col-xs-12 table-list-header hidden-xs">
-  <div class="row">
-    <div class="col-sm-8 col">
-      Name <a href="#"><small><i class="fa fa-chevron-down"></i></small></a>
-    </div>
-    <div class="col-sm-3 col">
-      Display Order <a href="#"><small><i class="fa fa-chevron-down"></i></small></a>
-    </div>
-  </div>
-</div>
-<?php foreach($rows as $row): ?>
-  <?php $prms['cid'] = $row[0]; if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
-  <div class="col-xs-12 table-list-row">
+<?php if(sizeof($rows) > 0): ?>
+  <div class="col-xs-12 table-list-header hidden-xs">
     <div class="row">
-      <div class="col-xs-12 col-sm-8 table-list-row-item">
-          <div class="col-xs-4 visible-xs helper-row">
-            <div class="row">Name</div>
-          </div>
-          <div class="col-xs-8 col-sm-12">
-            <div class="row"><?= $row[1] ?></div>
-          </div>
+      <div class="col-sm-8 col">
+        Name <a href="#"><small><i class="fa fa-chevron-down"></i></small></a>
       </div>
-      <div class="col-xs-12 col-sm-3 table-list-row-item">
-          <div class="col-xs-4 visible-xs helper-row">
-            <div class="row">Display Order</div>
-          </div>
-          <div class="col-xs-8 col-sm-12">
-            <div class="row"><?= $row[2] ?></div>
-          </div>
-      </div>
-      <div class="col-xs-12 col-sm-1 col-md-1 text-right action-buttons">
-        <a data-modify href="<?= _A_::$app->router()->UrlTo('categories/edit', $prms) ?>">
-          <i class="fa fa-pencil"></i>
-        </a>
-        <a href="<?= _A_::$app->router()->UrlTo('categories/delete', $prms) ?>" data-delete rel="nofollow"
-           class="text-danger <?= ($row[2]>0)?'disabled':''?>">
-          <i class=" fa fa-trash-o"></i>
-        </a>
+      <div class="col-sm-3 col">
+        Display Order <a href="#"><small><i class="fa fa-chevron-down"></i></small></a>
       </div>
     </div>
   </div>
-<?php endforeach; ?>
+  <?php foreach($rows as $row): ?>
+    <?php $prms['cid'] = $row[0]; if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
+    <div class="col-xs-12 table-list-row">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 table-list-row-item">
+            <div class="col-xs-4 visible-xs helper-row">
+              <div class="row">Name</div>
+            </div>
+            <div class="col-xs-8 col-sm-12">
+              <div class="row"><?= $row[1] ?></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-3 table-list-row-item">
+            <div class="col-xs-4 visible-xs helper-row">
+              <div class="row">Display Order</div>
+            </div>
+            <div class="col-xs-8 col-sm-12">
+              <div class="row"><?= $row[2] ?></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-1 col-md-1 text-right action-buttons">
+          <a data-modify href="<?= _A_::$app->router()->UrlTo('categories/edit', $prms) ?>">
+            <i class="fa fa-pencil"></i>
+          </a>
+          <a href="<?= _A_::$app->router()->UrlTo('categories/delete', $prms) ?>" data-delete rel="nofollow"
+             class="text-danger <?= ($row[2]>0)?'disabled':''?>">
+            <i class=" fa fa-trash-o"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+<?php else: ?>
+  <div class="col-sm-12 text-center offset-top">
+    <h2 class="offset-top">No results found</h2>
+  </div>
+<?php endif; ?>
 
--- views/product/rows.php
+++ views/product/rows.php
@@ -1,38 +1,41 @@
-<ul class="products">
-  <?php foreach($rows as $row): ?>
-    <?php $prms['pid'] = $row[0];
-    if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
-    <li
-      class="last product type-product status-publish has-post-thumbnail product_cat-brooches product_tag-fashion product_tag-jewelry sale featured shipping-taxable purchasable product-type-simple product-cat-brooches product-tag-fashion product-tag-jewelry instock">
-      <div class="product-inner">
-        <a href="<?= _A_::$app->router()->UrlTo('product/edit', $prms); ?>">
-          <figure class="product-image-box">
-            <a href="#">
-              <img width="#" height="266" src="<?= $row['filename']; ?>"
-                   class="attachment-shop_catalog size-shop_catalog wp-post-image" alt=""/>
-            </a>
-            <figcaption>
-              <a data-delete href="<?= _A_::$app->router()->UrlTo('product/delete', $prms); ?>" rel="nofollow"
-                 class="button icon-delete add_to_cart_button   product_type_simple"></a>
-              <a data-modify href="<?= _A_::$app->router()->UrlTo('product/edit', $prms); ?>"
-                 class="button product-button icon-modify">
+<?php if(sizeof($rows) > 0): ?>
+  <ul class="products">
+    <?php foreach($rows as $row): ?>
+      <?php $prms['pid'] = $row[0];
+      if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
+      <li
+        class="last product type-product status-publish has-post-thumbnail product_cat-brooches product_tag-fashion product_tag-jewelry sale featured shipping-taxable purchasable product-type-simple product-cat-brooches product-tag-fashion product-tag-jewelry instock">
+        <div class="product-inner">
+          <a href="<?= _A_::$app->router()->UrlTo('product/edit', $prms); ?>">
+            <figure class="product-image-box">
+              <a href="#">
+                <img width="#" height="266" src="<?= $row['filename']; ?>"
+                     class="attachment-shop_catalog size-shop_catalog wp-post-image" alt=""/>
               </a>
-            </figcaption>
-          </figure>
-
-          <span class="product-category"><?= $row['pname']; ?></span>
-
-          <h3><?= $row[8]; ?></h3>
-
-          <div class="product-price-box clearfix">
-            <span class="price">
-              <ins>
-                  <span class="amount"><?= $row['format_price']; ?></span>
-              </ins>
-            </span>
-          </div>
-        </a>
-      </div>
-    </li>
-  <?php endforeach; ?>
-</ul>
+              <figcaption>
+                <a data-delete href="<?= _A_::$app->router()->UrlTo('product/delete', $prms); ?>" rel="nofollow"
+                   class="button icon-delete add_to_cart_button   product_type_simple"></a>
+                <a data-modify href="<?= _A_::$app->router()->UrlTo('product/edit', $prms); ?>"
+                   class="button product-button icon-modify">
+                </a>
+              </figcaption>
+            </figure>
+            <span class="product-category"><?= $row['pname']; ?></span>
+            <h3><?= $row[8]; ?></h3>
+            <div class="product-price-box clearfix">
+              <span class="price">
+                <ins>
+                    <span class="amount"><?= $row['format_price']; ?></span>
+                </ins>
+              </span>
+            </div>
+          </a>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php else: ?>
+  <div class="col-sm-12 text-center offset-top">
+    <h2 class="offset-top">No results found</h2>
+  </div>
+<?php endif; ?>
