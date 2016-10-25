976653c53bbbffce36aa6f3aadf3971559522b00
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-10-25 16:02:26 +0300

    users profile form

--- views/admin/edit.php
+++ views/admin/edit.php
@@ -1,11 +1,22 @@
 <div class="container">
   <div id="content" class="main-content-inner" role="main">
-    <a href="<?= $back_url; ?>" class="button back_button">Back</a>
-
-    <div style="padding-top: 20px; margin: auto; width: 600px;">
-      <div id="form_content">
-        <?= $form; ?>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="row afterhead-row">
+          <div class="col-xs-12 back_button_container">
+            <a href="<?= $back_url; ?>" class="button back_button">Back</a>
+          </div>
+          <div class="col-xs-12 text-center">
+            <div class="row">
+              <h3 class="page-title"><?= $form_title ?></h3>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+
+    <div id="form_content" class="row">
+      <?= $form; ?>
     </div>
   </div>
 </div>
--- views/authorization/authorization.php
+++ views/authorization/authorization.php
@@ -1,52 +1,48 @@
 <div class="container">
   <div class="row">
-    <div class="col-md-12">
-      <article class="page type-page status-publish entry">
-        <br>
-        <br>
-        <h1 class="entry-title">Account</h1>
-        <div class="entry-content">
-          <div class="woocommerce">
-            <h2>Login</h2>
+    <div class="col-md-8 col-md-push-2 col-xs-12">
+      <div class="col-md-12 col-xs-12 text-center">
+        <h1 class="entry-title">Login</h1>
+      </div>
+      <div class="col-md-12 col-xs-12 entry-content">
+        <div class="woocommerce">
+          <form method="post" id="authorization" action="<?= _A_::$app->router()->UrlTo('authorization'); ?>"
+                class="login">
+            <input type="hidden" name="redirect"
+                   value="<?= isset($redirect) ? $redirect : _A_::$app->router()->UrlTo('/'); ?>"/>
 
-            <form method="POST" id="authorization" action="<?= _A_::$app->router()->UrlTo('authorization'); ?>"
-                  class="login">
-              <input type="hidden" name="redirect"
-                     value="<?= isset($redirect) ? $redirect : _A_::$app->router()->UrlTo('/'); ?>"/>
+            <div class="form-row">
+              <label for="username">Email Address/Username <span class="required">*</span></label>
+              <input type="text" class="input-text" name="login" id="username" value=""/>
+            </div>
 
-              <p class="form-row form-row-wide">
-                <label for="username">Email Address/Username <span class="required">*</span></label>
-                <input type="text" class="input-text" name="login" id="username" value=""/>
-              </p>
+            <div class="form-row form-row-wide">
+              <label for="password">Password <span class="required">*</span></label>
+              <input class="input-text" type="password" name="pass" id="password"/>
+            </div>
 
-              <p class="form-row form-row-wide">
-                <label for="password">Password <span class="required">*</span></label>
-                <input class="input-text" type="password" name="pass" id="password"/>
-              </p>
+            <div class="form-row">
+              <input type="hidden" id="_wpnonce" name="_wpnonce" value="c0312ae7bb"/>
+              <input type="hidden" name="_wp_http_referer" value="#"/>
+              <input id="blogin" type="button" class="button" name="login"
+                     data-action="<?= _A_::$app->router()->UrlTo('authorization'); ?>" value="Login"/>
+              <label for="rememberme" class="inline">
+                <input name="rememberme" value="1" type="checkbox" id="rememberme"/>
+                Remember me
+              </label>
+            </div>
 
-              <p class="form-row">
-                <input type="hidden" id="_wpnonce" name="_wpnonce" value="c0312ae7bb"/>
-                <input type="hidden" name="_wp_http_referer" value="#"/>
-                <input id="blogin" type="button" class="button" name="login"
-                       data-action="<?= _A_::$app->router()->UrlTo('authorization'); ?>" value="Login"/>
-                <label for="rememberme" class="inline">
-                  <input name="rememberme" value="1" type="checkbox" id="rememberme"/>
-                  Remember me
-                </label>
-              </p>
+            <div class="lost_password">
+              <a id="lost_password" href="<?= $lostpassword_url ?>">Lost your password?</a>
+              <a id="register_user" href="<?= $registration_url ?>" style="float: right;">Registration</a>
+            </div>
 
-              <p class="lost_password">
-                <a id="lost_password" href="<?= $lostpassword_url ?>">Lost your password?</a>
-                <a id="register_user" href="<?= $registration_url ?>" style="float: right;">Registration</a>
-              </p>
-
-              <div class="text-center">
-                <div class="results" style="color: red;"></div>
-              </div>
-            </form>
-          </div>
+            <div class="text-center">
+              <div class="results" style="color: red;"></div>
+            </div>
+          </form>
         </div>
-      </article>
+      </div>
     </div>
   </div>
 </div>
