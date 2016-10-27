849d58aa04a1d81158ab296ee84d60e458626fe7
Gregory Popov <greg.georg.popov@gmail.com>
2016-10-27 14:05:15 +0300

    UI fixe 2

--- views/messages/alert-boxes.php
+++ views/messages/alert-boxes.php
@@ -1,15 +1,20 @@
-<div class="col-xs-12">
-  <?php if (isset($warning)) { ?>
-    <div class="col-xs-12 alert-success danger">
-      <?php foreach ($warning as $msg) {
-        echo $msg . "<br/>";
-      } ?>
+<div class="col-xs-12 alert-container">
+    <div class="row">
+      <?php if (isset($warning)) { ?>
+        <div class="col-xs-12 alert-success danger">
+          <?php foreach ($warning as $msg) {
+            echo $msg . "<br/>";
+          } ?>
+        </div>
+      <?php } if (isset($error)) { ?>
+        <div class="col-xs-12 alert-danger danger">
+          <?php foreach ($error as $msg) {
+            echo $msg . "<br/>";
+          } ?>
+        </div>
+      <?php } ?>
+        <div class="col-xs-12 alert-success danger">
+            Example of notification
+        </div>
     </div>
-  <?php } if (isset($error)) { ?>
-    <div class="col-xs-12 alert-danger danger">
-      <?php foreach ($error as $msg) {
-        echo $msg . "<br/>";
-      } ?>
-    </div>
-  <?php } ?>
 </div>
