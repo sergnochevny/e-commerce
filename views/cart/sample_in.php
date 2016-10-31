4269ca41bd03db194a9323f67fdc0ba811d275af
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-10-31 19:24:52 +0200

    shop changes ..

--- views/cart/sample_in.php
+++ views/cart/sample_in.php
@@ -2,13 +2,15 @@
   <div class="col-sm-6">
     <div class="row">
       <div class="col-sm-4">
-        <a href="<?= _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart']); ?>">
-          <img alt="" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image"
-               src="<?= $img_url; ?>">
+        <?php
+          $href = _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart'], $item['Product_name']);
+        ?>
+        <a href="<?= $href; ?>">
+          <img alt="" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" src="<?= $img_url; ?>">
         </a>
       </div>
       <div class="col-sm-8">
-        <a href="<?= _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart']); ?>">
+        <a href="<?= $href; ?>">
           <?= $item['Product_name']; ?>
         </a>
       </div>
--- views/cart/sample_in_proceed.php
+++ views/cart/sample_in_proceed.php
@@ -2,13 +2,15 @@
   <div class="col-sm-10">
     <div class="row">
       <div class="col-sm-2">
-        <a href="<?= _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart']); ?>">
-          <img class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image"
-               src="<?= $img_url; ?>">
+        <?php
+          $href = _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart'], $item['Product_name']);
+        ?>
+        <a href="<?= $href; ?>">
+          <img class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" src="<?= $img_url; ?>">
         </a>
       </div>
       <div class="col-sm-10">
-        SAMPLE - <a href="<?= _A_::$app->router()->UrlTo('shop/product', ['pid' => $pid, 'back' => 'cart']); ?>">
+        SAMPLE - <a href="<?= $href; ?>">
           <?= $item['Product_name']; ?>
         </a>
       </div>
