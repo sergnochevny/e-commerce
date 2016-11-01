9e32ba478f237a0174b849536590e85be6af4f18
Gregory Popov <greg.georg.popov@gmail.com>
2016-11-01 16:36:23 +0200

    About fix  2

--- views/index/static/about.php
+++ views/index/static/about.php
@@ -1,42 +1,43 @@
 <?php include('views/index/main_gallery.php'); ?>
   <div id="static">
     <div class="container">
+
       <div class="row">
         <div class="col-md-12">
-          <article class="page type-page status-publish entry">
-            <div class="entry-content">
-              <div class="vc_row wpb_row vc_row-fluid vc_custom_1439733758005">
-                <div class="wpb_column vc_column_container vc_col-sm-12">
-                  <div class="wpb_wrapper">
-                    <div class="just-divider text-left line-no icon-hide">
-                      <div class="divider-inner" style="background-color: #fff">
-                        <h3 class="just-section-title">About Us</h3>
-                        <p class="paragraph">25 Years Selling Designer Fabrics</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div class="row">
 
-              <p>We travel the world to source out and bring you the finest, most exclusive and exquisite
+            <div class="col-xs-12 text-center afterhead-row">
+              <h2 class="page-title">About Us <br>
+                <small>25 Years Selling Designer Fabrics</small>
+              </h2>
+            </div>
+
+            <div class="col-xs-12">
+              <p class="text-justify">
+                We travel the world to source out and bring you the finest, most exclusive and exquisite
                 fabrics available, many of which are commonly available only to the top design trade, and we
-                sell them below trade wholesale.</p>
+                sell them below trade wholesale.
+              </p>
 
-              <p>Over the years iLuvFabix.com and our designer with over 25 years in the high end Design
-                Marketplace, has acquired a client base second to none.</p>
+              <p class="text-justify">
+                Over the years iLuvFabix.com and our designer with over 25 years in the high end Design
+                Marketplace, has acquired a client base second to none.
+              </p>
 
-              <p>As a special service to our purchasing customers, iluvfabrix.com is proud to provide
+              <p class="text-justify">
+                As a special service to our purchasing customers, iluvfabrix.com is proud to provide
                 Professional Design Services AT NO COST. Our designer, upon request will group fabrics
                 together FOR YOUR HOME and email you the pictures. In this way you not only get access to
-                the “Best of the Best” fabrics Below Trade Wholesale, you get the services of a Professional
+                the &laquo;Best of the Best&raquo; fabrics Below Trade Wholesale, you get the services of a Professional
                 Designer without Paying the Hourly Fee.
               </p>
-              <p><b>There is no better high end fabric deal on the web.</b></p>
-              <div class="vc_row-full-width"></div>
             </div>
-          </article>
+
+
+          </div>
         </div>
       </div>
+
     </div>
   </div>
   <script src='<?= _A_::$app->router()->UrlTo('views/js/static/static.js'); ?>' type="text/javascript"></script>
