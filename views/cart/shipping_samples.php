70cb1d982b55e63fb344635793b5e68a3ec3850f
Gregory Popov <greg.georg.popov@gmail.com>
2016-11-01 15:29:05 +0200

    Cart minor fixes 2

--- views/cart/shipping_samples.php
+++ views/cart/shipping_samples.php
@@ -1,50 +1,49 @@
-<th>Shipping</th>
-<td data-title="Shipping">
-    <p>
-    <table>
-        <tbody>
-            <?php
-                if ($systemAllowExpressSamples) { ?>
-                    <?php
-                        #if the user has not selected express post, show the teaser.  if they have then dont show it
-                        if (!$bExpressSamples) {
-                    ?>
-                        <tr>
-                            <td valign="middle" class="copyProducts">
-                                Express Post... generally delivered within 5-7 days
-                            </td>
-                        </tr>
-                    <?php } ?>
-                    <tr align="left" valign="middle">
-                        <td valign="middle" class="viewDetails">
-                            <input id="express_samples" type="checkbox" name="express_samples"
-                                   value="1" <?= ($bExpressSamples) ? 'checked' : ''; ?>>
-                            DELIVER MY SAMPLES BY OVERNIGHT COURIER<br>
-                            &nbsp;<span color="#663300">$<?= number_format(SAMPLES_PRICE_EXPRESS_SHIPPING, 2); ?> USD
-                                surcharge</span>
-                            <?php if ($bExpressSamples) { ?>
-                                <p style="font-size:80%;">
-                                    <b>NOTE: </b>
-                                    Only orders placed Monday through Thursday prior to 2:00 PM Eastern time will be processed
-                                    for overnight delivery. Orders placed outside those hours will be processed the next open
-                                    business day.
-                                </p>
-                            <?php } ?>
-                        </td>
-                    </tr>
-            <?php } if ($bExpressSamples) { ?>
-
-                    <tr align="left" valign="middle">
-                        <td valign="middle" class="viewDetails" colspan="4" style="color: #000000;">
-                            <input id="accept_express" type="checkbox" name="accept_express" value="1" <?=($bAcceptExpress)?'checked':'';?>>
-                            <span style="font-size: 80%;">
-                                I acknowledge that in rare cases samples may not arrive overnight due to circumstances beyond the control of iLuvFabrix.com. There are no guarantees or refunds given for this service.
-                            <span>
-                        </td>
-                    </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    </p>
-</td>
-
+<div class="col-sm-12">
+  <div class="row">
+    <div class="col-sm-12">
+      <h4><b>Shipping</b></h4>
+    </div>
+  </div>
+  <?php if ($systemAllowExpressSamples): if (!$bExpressSamples): ?>
+    <div class="row">
+      <div class="col-sm-12 inner-offset-bottom">
+        Express Post... generally delivered within 5-7 days
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 inner-offset-bottom">
+        <div class="form-row">
+          <label for="express_samples" class="inline">
+            <input id="express_samples" type="checkbox" name="express_samples"
+                   value="1" <?= ($bExpressSamples) ? 'checked' : ''; ?>>
+            DELIVER MY SAMPLES BY OVERNIGHT COURIER
+          </label>
+        </div>
+        &nbsp;<span color="#663300">$<?= number_format(SAMPLES_PRICE_EXPRESS_SHIPPING, 2); ?> USD
+                    surcharge</span>
+        <?php if ($bExpressSamples) { ?>
+          <small style="color:#999;">
+            <b>NOTE: </b>
+            Only orders placed Monday through Thursday prior to 2:00 PM Eastern time will be processed
+            for overnight delivery. Orders placed outside those hours will be processed the next open
+            business day.
+          </small>
+        <?php } ?>
+      </div>
+    </div>
+  <?php endif; endif; ?>
+  <?php if ($bExpressSamples): ?>
+    <div class="row">
+      <div class="col-sm-12 inner-offset-bottom">
+        <div class="form-row">
+          <label class="inline">
+            <input id="accept_express" type="checkbox" name="accept_express"
+                   value="1" <?= ($bAcceptExpress) ? 'checked' : ''; ?>>
+            I acknowledge that in rare cases samples may not arrive overnight due to circumstances beyond the control of
+            iLuvFabrix.com. There are no guarantees or refunds given for this service.
+          </label>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+</div>
