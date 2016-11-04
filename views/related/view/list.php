0dd5a8beda8ee03525da977ede526ed50f2aeb12
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-11-04 15:15:13 +0200

    DB dump

--- views/related/view/list.php
+++ views/related/view/list.php
@@ -1,10 +1,12 @@
-<div class="col-xs-12 text-center afterhead-row">
-  <h3 class="page-title">Related Products</h3>
-</div>
-<div class="row">
-  <div class="col-xs-12 data-view">
-    <div class="row products">
-      <?= $list; ?>
+<?php if(!empty($list)): ?>
+  <div class="col-xs-12 text-center afterhead-row">
+    <h3 class="page-title">Related Products</h3>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 data-view">
+      <div class="row products">
+        <?= $list; ?>
+      </div>
     </div>
   </div>
-</div>
+<?php endif; ?>
--- views/related/view/rows.php
+++ views/related/view/rows.php
@@ -1,32 +1,46 @@
 <?php if(count($rows) > 0): ?>
   <?php foreach($rows as $row): ?>
-    <?php $prms['pid'] = $row[0];
-    if(!is_null(_A_::$app->get('page'))) $prms['page'] = _A_::$app->get('page'); ?>
-    <div class="col-xs-12 col-sm-6 col-md-4 product-item">
+    <div class="product-item">
       <div class="product-inner">
-        <figure class="product-image-box" style="background-image: url(<?= $row['filename']; ?>)">
-          <figcaption>
-            <a data-waitloader data-delete href="<?= _A_::$app->router()->UrlTo('product/delete', $prms); ?>" rel="nofollow"
-               class="button icon-delete add_to_cart_button   product_type_simple"></a>
-            <a data-waitloader data-modify href="<?= _A_::$app->router()->UrlTo('product/edit', $prms); ?>"
-               class="button product-button icon-modify">
-            </a>
-          </figcaption>
+        <?php
+          $url_prms['pid'] = $row[0];
+          $url_prms['back'] = '';
+          $href = _A_::$app->router()->UrlTo('shop/product', $url_prms, $row['pname'], ['cat', 'mnf', 'ptrn']);
+        ?>
+        <figure class="product-image-box" style="background-image:url(<?= $row['filename']; ?>)">
+          <a data-waitloader href="<?= $href; ?>">
+            <?php if($row['bProductDiscount']) { ?>
+              <span class="extra_discount">Extra Discount!</span>
+            <?php } ?>
+            <figcaption>
+              <?php
+                if($row['in_cart']) {
+                  include('views/cart/basket.php');
+                } else {
+                  ?>
+                  <a data-waitloader class="button add-to-basket" href="<?= $href; ?>">View Details</a>
+                  <?php
+                }
+              ?>
+            </figcaption>
+          </a>
         </figure>
-        <span class="product-category"><?= $row['pname']; ?></span>
-        <p class="description"><?= $row['ldesc']; ?></p>
+
+        <span class="on-sale">New!</span>
+        <span class="product-category"><a href="<?= $href; ?>"><?= $row['pname']; ?></a></span>
+        <p class="description">
+          <?= (strlen($row['sdesc']) > 0) ? $row['sdesc'] : $row['ldesc']; ?>
+        </p>
         <div class="product-price-box clearfix">
-            <span class="price">
-              <ins>
-                  <span class="amount"><?= '$' . $row['format_price']; ?></span>
-              </ins>
-            </span>
+          <?php if($row['sys_hide_price'] == 0 && $row['hideprice'] == 0) { ?>
+            <span class="price pull-left"><ins><span class="amount"><?= $row['format_price']; ?></span></ins></span>
+          <?php }
+            if(isset($row['saleprice']) && ($row['price'] != $row['saleprice'])) { ?>
+              <span class="text-sale pull-right" style="float:right;color: red;">Sale: <ins><span
+                    class="amount_wd"><?= $row['format_sale_price']; ?></span></ins></span>
+            <?php } ?>
         </div>
       </div>
     </div>
   <?php endforeach; ?>
-<?php else: ?>
-  <div class="col-sm-12 text-center offset-top">
-    <h2 class="offset-top page-title">No results found</h2>
-  </div>
 <?php endif; ?>
