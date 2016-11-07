c98593fc8b798a09cacd93c139337f0faf3cf650
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-11-07 18:56:37 +0200

    related add ....

--- views/related/list.php
+++ views/related/list.php
@@ -1,53 +1,53 @@
 <?php
   $prms = null;
-  if (!is_null(_A_::$app->get('page'))) {
+  if(!is_null(_A_::$app->get('page'))) {
     $prms['page'] = _A_::$app->get('page');
   }
 ?>
 <div class="col-xs-12">
-<div class="col-xs-12 text-center">
-  <b class="h3">Select Related Fabrics</b>
-</div>
+  <div class="col-xs-12 text-center">
+    <b class="h3">Select Related Fabrics</b>
+  </div>
 
-<?= isset($search_form) ? $search_form : '' ?>
+  <?= isset($search_form) ? $search_form : '' ?>
 
-<div class="row">
-  <div class="col-xs-12 search-result-header">
+  <div class="row">
+    <div class="col-xs-12 search-result-header">
 
-    <div class="row">
-      <div class="col-sm-6 col-sm-offset-6 search-result-container text-right">
-        <span class="search-result">Showing <?= $count_rows; ?> results</span>
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-6 search-result-container text-right">
+          <span class="search-result">Showing <?= $count_rows; ?> results</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 data-view">
+      <div class="row products">
+        <?= $list; ?>
       </div>
     </div>
-
   </div>
-</div>
 
-<div class="row">
-  <div class="col-xs-12 data-view">
-    <div class="row products">
-      <?= $list; ?>
+  <div class="row">
+    <div class="col-xs-12">
+
+      <nav class="paging-navigation" role="navigation">
+        <h4 class="sr-only">Navigation</h4>
+        <ul class="pagination">
+          <?= isset($paginator) ? $paginator : ''; ?>
+        </ul>
+      </nav>
+
     </div>
   </div>
-</div>
-
-<div class="row">
-  <div class="col-xs-12">
-
-    <nav class="paging-navigation" role="navigation">
-      <h4 class="sr-only">Navigation</h4>
-      <ul class="pagination">
-        <?= isset($paginator) ? $paginator : ''; ?>
-      </ul>
-    </nav>
-
-  </div>
-</div>
-<div class="row">
-  <div class="col-xs-12">
-    <div class="text-center">
-      <a data-related_add_ok class="button" style="width: 150px;">Ok</a>
-    </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="text-center">
+        <a data-related_add_ok class="button" style="width: 150px;">Ok</a>
+      </div>
     </div>
   </div>
 </div>
