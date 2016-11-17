7a0d3da686e62299288a390fd87a368edd1d34b4
Serg Nochevny <serg.nochevny@engineering.ait.com>
2016-11-17 12:30:47 +0200

    ui changes .....

--- views/menu/menu.php
+++ views/menu/menu.php
@@ -1,35 +1,54 @@
-<div class="col-md-10 col-lg-10">
-  <div class="navbar-collapse collapse navbar-collapse-top">
-    <ul id="menu-header-menu" class="site-menu nav navbar-nav navbar-right">
+<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="row pull-right">
+        <a class="button" title="Ask a Question" href="mailto:info@iluvfabrix.com">
+          Ask a Question
+        </a>
+      </div>
+    </div>
 
-      <li
-        class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item current_page_item active">
-        <a data-waitloader data-link title="Home" href="<?= _A_::$app->router()->UrlTo('/') ?>">Home</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Shop" href="<?= _A_::$app->router()->UrlTo('shop'); ?>">Shop</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Blog" aria-haspopup="true" class="has-submenu"
-           href="<?= _A_::$app->router()->UrlTo('blog/view') ?>">Blog<span class="caret"></span></a>
-        <?= isset($blog_menu) ? $blog_menu : ''; ?>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Newsletter" href="<?= _A_::$app->router()->UrlTo('newsletter') ?>">Newsletter</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Service" href="<?= _A_::$app->router()->UrlTo('service') ?>">Service</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="About us" href="<?= _A_::$app->router()->UrlTo('about') ?>">About us</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Contact" href="<?= _A_::$app->router()->UrlTo('contact') ?>">Contact</a>
-      </li>
-      <li class="menu-item menu-item-type-post_type menu-item-object-page">
-        <a data-waitloader data-link title="Matches" href="<?= _A_::$app->router()->UrlTo('matches') ?>">Matches</a>
-      </li>
+    <div class="col-xs-12 navbar-toggle navbar-icon toggle-menu" data-toggle="collapse"
+         data-target=".site-navigation .navbar-collapse" id="menu-button">
+      <div class="row pull-right">
+        <div class="hamburger">
+          <div class="inner"></div>
+        </div>
+      </div>
+    </div>
 
-    </ul>
+    <div class="col-xs-12 pull-right navbar-collapse collapse navbar-collapse-top">
+      <div class="row">
+        <ul id="menu-header-menu" class="site-menu nav navbar-nav">
+          <li
+            class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item current_page_item active">
+            <a data-waitloader data-link title="Home" href="<?= _A_::$app->router()->UrlTo('/') ?>">Home</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Shop" href="<?= _A_::$app->router()->UrlTo('shop'); ?>">Shop</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Blog" aria-haspopup="true" class="has-submenu"
+               href="<?= _A_::$app->router()->UrlTo('blog/view') ?>">Blog<span class="caret"></span></a>
+            <?= isset($blog_menu) ? $blog_menu : ''; ?>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Newsletter" href="<?= _A_::$app->router()->UrlTo('newsletter') ?>">Newsletter</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Service" href="<?= _A_::$app->router()->UrlTo('service') ?>">Service</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="About us" href="<?= _A_::$app->router()->UrlTo('about') ?>">About us</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Contact" href="<?= _A_::$app->router()->UrlTo('contact') ?>">Contact</a>
+          </li>
+          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+            <a data-waitloader data-link title="Matches" href="<?= _A_::$app->router()->UrlTo('matches') ?>">Matches</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </div>
