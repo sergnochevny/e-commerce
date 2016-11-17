ddf9e64e6ebf208e206e98d59627fe770fb8fc60
Vadim Aminov <alexander.rybalka@engineering.ait.com>
2016-11-17 14:09:58 +0200

    new service page

---
+++ views/index/static/estimator.php
@@ -0,0 +1,13 @@
+<?php include('views/index/main_gallery.php'); ?>
+
+    <div id="static">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src='<?= _A_::$app->router()->UrlTo('views/js/static/static.js'); ?>' type="text/javascript"></script>
+<?php include('views/index/block_footer.php'); ?>
--- views/index/static/service.php
+++ views/index/static/service.php
@@ -3,99 +3,85 @@
   <div id="static">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <article class="page type-page status-publish entry">
-            <div class="entry-content">
-              <div class="vc_row wpb_row vc_row-fluid vc_custom_1439733758005">
-                <div class="wpb_column vc_column_container vc_col-sm-12">
-                  <div class="wpb_wrapper">
-                    <div class="just-divider text-left line-no icon-hide">
-                      <div class="divider-inner">
-                        <h3 class="just-section-title">Customer service</h3>
+        <div class="col-xs-12">
 
-                        <p class="paragraph">Please feel free to contact us with your questions
-                          or comments. Simply fill out the form below and one of our
-                          representatives will contact you shortly. </p>
-                      </div>
-                    </div>
+          <div class="service">
+            <h2 class="service-title text-center">Customer service</h2>
+            <p class="service-desc">Please feel free to contact us with your questions or comments. Simply fill out the
+              form below and one of our representatives will contact you shortly.</p>
+            <div class="service__cont">
+              <div class="row">
+                <div class="col-sm-6">
+                  <h3 class="service__cont-title">
+                    Shipping
+                  </h3>
+                  <p class="service__cont-desc">
+                    <span>We ship Worldwide.</span>
+                    Products are generally shipped within 24 to 72 hours from the time of approved payment received.
+                  </p>
+                </div>
+                <div class="col-sm-6">
+                  <h3 class="service__cont-title">
+                    Sales Terms
+                  </h3>
+                  <p class="service__cont-desc">
+                    <span>Please purchase a sample (we offer samples at very reasonable prices) if you are not entirely confident in your fabric purchase, as there are no returns or refunds.</span>
+                    We cannot guarantee dye lot if a sample is purchased more than 2 weeks prior to the purchase of your order. Dye lots are normally close but each dye lot is different.
+                  </p>
+                </div>
+              </div>
+
+
+              <h3 class="service__cont-title">
+                All Sales are Final
+              </h3>
+              <p class="service__cont-desc">
+                <span>All special orders are final sale. No refunds. No cancellations. No returns.</span>
+                <span>Please <a href="<?= _A_::$app->router()->UrlTo('contact'); ?>">contact</a> us for information on fabric sample purchases.</span>
+                <span>We are confident you'll find our products to be of the highest quality. If however your are not completely satisfied with a product, please contact us by email or phone and we will endeavor to rectify the problem.</span>
+                Should ILuvFabrix.com accept a return (at ILuvFabrix's sole discretion), it must be accompanied by a return authorization number. No fabric is to be returned without a Return Authorization Number. Any fabric returned without such a number will not be accepted. If a return is authorized by iluvfabrix or Michelle’s Fabrics, all return shipping costs and any customs duties or taxes are to be paid in their entirety by the purchaser, unless otherwise agreed to in advance of the return by iluvfabrix or Michelle’s Fabrics. Please examine your package immediately upon receipt so as to ensure the integrity of its contents. Please contact us by email at:
+                <span><a href="mailto:info@iluvfabrix.com">info@iluvfabrix.com</a> <br>
+                or phone at: <a href="tel:9155870200">(915) 587-0200</a></span>
+              </p>
+              <div class="row">
+                <div class="col-sm-6">
+                  <h3 class="service__cont-title">
+                    Privacy Policy
+                  </h3>
+                  <p class="service__cont-desc">
+                    Your personal information is never under any circumstances shared with any other individuals or organizations.
+                    <a href="<?= _A_::$app->router()->UrlTo('privacy'); ?>">privacy policy.</a>
+                  </p>
+                </div>
+                <div class="col-sm-6">
+                  <h3 class="service__cont-title">
+                    Security Information
+                  </h3>
+                  <p class="service__cont-desc">
+                    All purchase transactions made on our site are fully and completely secured.
+                  </p>
+                </div>
+              </div>
+
+              <h3 class="service__cont-title">
+                Contact Information
+              </h3>
+              <div class="service__cont-desc">
+                <div class="row">
+                  <div class="col-sm-4">
+                    <span>iluvfabrix (Division of Fabric Love LLC)</span>
+                    <span>211 Teramar Way</span>
+                    <span>El Paso, Texas, 79922</span>
+                    <span>United States</span>
+                  </div>
+                  <div class="col-sm-3">
+                    Email: <a href="mailto:info@iluvfabrix.com">info@iluvfabrix.com</a> <br>
+                    Tel: <a href="tel:9155870200">(915) 587-0200</a>
                   </div>
                 </div>
               </div>
-              <p>Due to another technical error, the contact us form is offline. We will have it back up
-                as soon as possible. In the meantime, please <a href="<?= _A_::$app->router()->UrlTo('contact'); ?>">click
-                  here</a> to use
-                this method to contact us.</p>
-
-              <p><b>Thank you for yor patience!</b></p>
-              <hr>
-              <h3>Shipping</h3>
-
-              <p>We ship Worldwide.</p>
-
-              <p>Products are generally shipped within 24 to 72 hours from the time of approved payment
-                received. </p>
-
-              <h3>Sales Terms</h3>
-
-              <p>Please purchase a sample (we offer samples at very reasonable prices) if you are not
-                entirely confident in your fabric purchase, as there are no returns or refunds.</p>
-
-              <p>We cannot guarantee dye lot if a sample is purchased more than 2 weeks prior to the
-                purchase of your order. Dye lots are normally close but each dye lot is different.</p>
-
-              <h3>All sales are final</h3>
-
-              <p>All special orders are final sale. No refunds. No cancellations. No returns.</p>
-
-              <p>Please <a href="<?= _A_::$app->router()->UrlTo('contact'); ?>">contact us</a> for information on fabric
-                sample purchases.
-              </p>
-
-              <p>We are confident you'll find our products to be of the highest quality. If however you
-                are not completely satisfied with a product, please contact us by email or phone and we
-                will endeavor to rectify the problem.</p>
-
-              <p>Should ILuvFabrix.com accept a return (at ILuvFabrix's sole discretion), it must be
-                accompanied by a return authorization number. No fabric is to be returned without a
-                Return Authorization Number. Any fabric returned without such a number will not be
-                accepted. If a return is authorized by iluvfabrix or Michelle’s Fabrics, all return
-                shipping costs and any customs duties or taxes are to be paid in their entirety by the
-                purchaser, unless otherwise agreed to in advance of the return by iluvfabrix or
-                Michelle’s Fabrics. Please examine your package immediately upon receipt so as to ensure
-                the integrity of its contents. Please contact us by email at: </p>
-
-              <p><a href="mailto:info@iluvfabrix.com" class="copyProducts">info@iluvfabrix.com</a> or
-                phone at: <a href="tel:9155870200">(915) 587-0200</a></p>
-
-              <div class="row">
-                <div class="col-sm-6">
-                  <h3>Privacy Policy</h3>
-
-                  <p>Your personal information is never under any circumstances shared with any other
-                    individuals or organizations.<br>
-                    <a href="<?= _A_::$app->router()->UrlTo('privacy'); ?>">privacy policy</a>.
-                  </p>
-
-                  <h3>Security Information</h3>
-
-                  <p>All purchase transactions made on our site are fully and completely secured.</p>
-                </div>
-                <div class="col-sm-6">
-                  <h3>Contact Information</h3>
-
-                  <p><strong>iluvfabrix</strong> (Division of Fabric Love LLC)<br>
-                    211 Teramar Way<br>
-                    El Paso, Texas, 79922<br>
-                    United States<br>
-                    <br>
-                    Email: <a href="mailto:info@iluvfabrix.com">info@iluvfabrix.com</a><br>
-                    Tel: <a href="tel:9155870200">(915) 587-0200</a>
-                  </p>
-                </div>
-              </div>
-              <div class="vc_row-full-width"></div>
             </div>
-          </article>
+          </div>
         </div>
       </div>
     </div>
