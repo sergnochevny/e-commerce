1f8ea99433826e41e44f5059ca25953c96391a26
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-01-05 12:04:48 +0200

    some fixes ..........

--- views/categories/view/rows.php
+++ views/categories/view/rows.php
@@ -3,7 +3,7 @@
     <?php $prms['cat'] = $row['cid']; ?>
     <div class="col-xs-6 col-sm-3 list-item">
       <div class="list-inner">
-        <a data-waitloader data-sb href="<?= _A_::$app->router()->UrlTo('shop', $prms, $row['cname']); ?>">
+        <a title="<?=$row['cname']?>" data-waitloader data-sb href="<?= _A_::$app->router()->UrlTo('shop', $prms, $row['cname']); ?>">
           <div class="item-name"><?= $row['cname']; ?></div>
         </a>
       </div>
