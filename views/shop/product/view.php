27b935aaebad477a1af54356e4bae7ef1cd08293
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-03-03 14:38:46 +0200

    some fixes.

--- views/shop/product/view.php
+++ views/shop/product/view.php
@@ -1,7 +1,7 @@
 <?php
   $pid = $data['pid'];
   /** @noinspection PhpUndefinedMethodInspection */
-  $ahref = 'mailto:'._A_::$app->keyStorage()->system_info_email.'?subject=' . rawurlencode($data['sdesc'] . ' ' . $data['pnumber']);
+  $ahref = 'mailto:' . _A_::$app->keyStorage()->system_info_email . '?subject=' . rawurlencode($data['sdesc'] . ' ' . $data['pnumber']);
   /** @noinspection PhpUndefinedMethodInspection */
   $mhref = _A_::$app->router()->UrlTo('matches/add', ['pid' => $pid]);
   /** @noinspection PhpUndefinedMethodInspection */
@@ -38,12 +38,12 @@
           <div class="col-xs-12 col-md-6">
             <div class="row">
               <a <?= isset($data['filename1']) ? 'href="' . $data['filename1'] . '"' : ''; ?>
-                itemprop="image" class="product-main-image zoom" title="Product Main Image"
+                  itemprop="image" class="product-main-image zoom" title="Product Main Image"
                 <?= isset($data['filename1']) ? 'data-rel="prettyPhoto[product-gallery]"' : ''; ?>
                 <?= isset($data['filename1']) ? 'data-img = "' . $data['filename1'] . '"' : '' ?>>
                 <img width="100%"
                      src="<?= isset($data['filename1']) ? $data['filename1'] : $data['filename']; ?>"
-                     class="attachment-shop_single size-shop_single wp-post-image" alt="" />
+                     class="attachment-shop_single size-shop_single wp-post-image" alt=""/>
               </a>
               <div class="thumbnails col-sm-12">
                 <div class="row">
@@ -58,42 +58,42 @@
 
                           if(!empty($data['image2'])) { ?>
                             <a <?= isset($data['img2_filename1']) ? 'href="' . $data['img2_filename1'] . '"' : ''; ?>
-                              class="col-xs-6 zoom"
+                                class="col-xs-6 zoom"
                               <?= isset($data['img2_filename1']) ? 'data-rel="prettyPhoto[product-gallery]"' : ''; ?>
                               <?= isset($data['img2_filename1']) ? 'data-img = "' . $data['img2_filename1'] . '"' : '' ?>>
                               <img width="100%" src="<?= $data['img2_filename']; ?>"
-                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt="" />
+                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt=""/>
                             </a>
                             <?php
                           }
 
                           if(!empty($data['image3'])) { ?>
                             <a <?= isset($data['img3_filename1']) ? 'href="' . $data['img3_filename1'] . '"' : ''; ?>
-                              class="col-xs-6 zoom"
+                                class="col-xs-6 zoom"
                               <?= isset($data['img3_filename1']) ? 'data-rel="prettyPhoto[product-gallery]"' : ''; ?>
                               <?= isset($data['img3_filename1']) ? 'data-img = "' . $data['img3_filename1'] . '"' : '' ?>>
                               <img width="100%" src="<?= $data['img3_filename']; ?>"
-                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt="" />
+                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt=""/>
                             </a>
                             <?php
                           }
                           if(!empty($data['image4'])) { ?>
                             <a <?= isset($data['img4_filename1']) ? 'href="' . $data['img4_filename1'] . '"' : ''; ?>
-                              class="zoom col-xs-6"
+                                class="zoom col-xs-6"
                               <?= isset($data['img4_filename1']) ? 'data-rel="prettyPhoto[product-gallery]"' : ''; ?>
                               <?= isset($data['img4_filename1']) ? 'data-img = "' . $data['img4_filename1'] . '"' : '' ?>>
                               <img width="100%" src="<?= $data['img4_filename']; ?>"
-                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt="" />
+                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt=""/>
                             </a>
                             <?php
                           }
                           if(!empty($data['image5'])) { ?>
                             <a <?= isset($data['img5_filename1']) ? 'href="' . $data['img5_filename1'] . '"' : ''; ?>
-                              class="zoom col-xs-6"
+                                class="zoom col-xs-6"
                               <?= isset($data['img5_filename1']) ? 'data-rel="prettyPhoto[product-gallery]"' : ''; ?>
                               <?= isset($data['img5_filename1']) ? 'data-img = "' . $data['img5_filename1'] . '"' : '' ?>>
                               <img width="100%" src="<?= $data['img5_filename']; ?>"
-                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt="" />
+                                   class="attachment-shop_thumbnail size-shop_thumbnail" alt=""/>
                             </a>
                             <?php
                           }
@@ -107,42 +107,40 @@
 
           <div class="col-xs-12 col-md-6 summary entry-summary">
             <div class="row">
-
-              <div class="panel panel-default panel-body">
-                <div class="col-xs-12">
-                  <div class="row">
-                    <p class="text-justify"><?= $data['ldesc']; ?></p>
-                  </div>
-                </div>
-                <div class="col-xs-12">
-                  <div class="row">
-                    <hr>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="row">
-                    <?php if($data['sys_hide_price'] == 0 && $data['hideprice'] == 0) { ?>
-                      <p class="price h4">Price:
-                        <ins>
-                        <span class="amount">
-                          <?= $data['format_price']; ?>
-                        </span>
-                        </ins>
-                      </p>
-                    <?php } ?>
-                  </div>
-                </div>
+              <div class="product_meta product_details sm-half-xs-full">
+                <span class="h3">Details</span>
+                <table class="table table-bordered table-striped">
+                  <tbody>
+                  <tr>
+                    <td class="row_title"><b>Name</b>:</td>
+                    <td><?= $data['pname']; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="row_title"><b>Product #</b>:</td>
+                    <td><?= $data['pnumber']; ?></td>
+                  </tr>
+                  <?php if(($data['piece'] == 1) && ($data['inventory'] > 0)) { ?>
+                    <tr>
+                      <td class="row_title"><b>Dimensions</b>:</td>
+                      <td><?= $data['dimensions']; ?></td>
+                    </tr>
+                  <?php } else { ?>
+                    <tr>
+                      <td class="row_title"><b>Width</b>:</td>
+                      <td><?= $data['width']; ?></td>
+                    </tr>
+                    <tr style="<?= ($data['inventory'] > 0) ? '' : 'color: red;'; ?>">
+                      <td class="row_title"><b>Avail. yardage</b>:</td>
+                      <td><?= $data['inventory'];
+                        ?></td>
+                    </tr>
+                  <?php } ?>
+                  </tbody>
+                </table>
               </div>
+            </div>
 
-              <div class="col-xs-12">
-                <div class="row"
-                     data-load="<?= /** @noinspection PhpUndefinedMethodInspection */
-                       _A_::$app->router()->UrlTo('info/view', ['method' => 'product']) ?>"></div>
-                <script type='text/javascript'
-                        src='<?= /** @noinspection PhpUndefinedMethodInspection */
-                          _A_::$app->router()->UrlTo('views/js/load.min.js'); ?>'></script>
-              </div>
-
+            <div class="row">
               <div class="col-xs-12">
                 <div class="row">
                   <table class="table table-bordered table-striped red">
@@ -157,7 +155,6 @@
               <div class="col-xs-12">
                 <div class="row">
                   <div class="product-detail-view-actions" aria-label="...">
-
                     <div class="row button-group-product">
                       <?php if($data['inventory'] > 0) { ?>
                         <div class="button-small-width">
@@ -230,49 +227,56 @@
                   </div>
                 </div>
               </div>
-            </div>
-
-            <div class="row">
-              <div class="product_meta product_details">
-                <h3>DETAILS</h3>
-                <table class="table table-bordered table-striped">
-                  <tbody>
-                  <tr>
-                    <td class="row_title"><b>Name</b>:</td>
-                    <td><?= $data['pname']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="row_title"><b>Product #</b>:</td>
-                    <td><?= $data['pnumber']; ?></td>
-                  </tr>
-                  <?php if(($data['piece'] == 1) && ($data['inventory'] > 0)) { ?>
-                    <tr>
-                      <td class="row_title"><b>Dimensions</b>:</td>
-                      <td><?= $data['dimensions']; ?></td>
-                    </tr>
-                  <?php } else { ?>
-                    <tr>
-                      <td class="row_title"><b>Width</b>:</td>
-                      <td><?= $data['width']; ?></td>
-                    </tr>
-                    <tr style="<?= ($data['inventory'] > 0) ? '' : 'color: red;'; ?>">
-                      <td class="row_title"><b>Avail. yardage</b>:</td>
-                      <td><?= $data['inventory'];
-                        ?></td>
-                    </tr>
-                  <?php } ?>
-                  </tbody>
-                </table>
+              <div class="col-xs-12 inner-offset-vertical-top">
+                <div class="row">
+                  <div class="panel panel-default panel-body">
+                    <div class="col-xs-12">
+                      <div class="row">
+                        <p class="text-justify"><?= $data['ldesc']; ?></p>
+                      </div>
+                    </div>
+                    <div class="col-xs-12">
+                      <div class="row">
+                        <hr>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="row">
+                        <?php if($data['sys_hide_price'] == 0 && $data['hideprice'] == 0) { ?>
+                          <p class="price h4">Price:
+                            <ins>
+                        <span class="amount">
+                          <?= $data['format_price']; ?>
+                        </span>
+                            </ins>
+                          </p>
+                        <?php } ?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
     </div>
 
-    <div class="col-xs-12">
-      <div class="row related" data-related>
-        <input data-href_related type="hidden" value="<?= $href_related; ?>"/>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="row related" data-related>
+          <input data-href_related type="hidden" value="<?= $href_related; ?>"/>
+        </div>
+      </div>
+
+      <div class="col-xs-12">
+        <div class="row"
+             data-load="<?= /** @noinspection PhpUndefinedMethodInspection */
+               _A_::$app->router()->UrlTo('info/view', ['method' => 'product']) ?>"></div>
+        <script type='text/javascript'
+                src='<?= /** @noinspection PhpUndefinedMethodInspection */
+                  _A_::$app->router()->UrlTo('views/js/load.min.js'); ?>'></script>
       </div>
     </div>
 
