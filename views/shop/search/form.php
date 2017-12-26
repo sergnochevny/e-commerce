12a5090cf5146b83e0a3566cbe2afe7a6dcd73bc
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-12-26 13:29:46 +0200

    ..............................

--- views/shop/search/form.php
+++ views/shop/search/form.php
@@ -4,7 +4,7 @@ use app\core\App;
 use controllers\ControllerAdmin;
 
 ?>
-<form action="<?= $action ?>" method="post" data-search class="col-xs-12">
+<form action="<?= $action ?>" method="post" data-search>
   <?php if(ControllerAdmin::is_logged()): ?>
     <div class="row">
       <div class="col-xs-12 panel panel-default search-panel">
@@ -191,47 +191,25 @@ use controllers\ControllerAdmin;
     </div>
 
   <?php else : ?>
-    <div class="row">
-      <div class="col-xs-12 panel panel-default search-panel">
-        <div class="panel-heading">
-          <div class="search-container-title">
-            <div class="row">
-              <div class="col-xs-1 col-sm-1"><i class="fa fa-2x fa-search"></i></div>
-              <div class="h4 col-xs-10 search-result-list comment-text">
-                <?php if(isset($search['a.pname'])): ?>
-                  <div class="label label-search-info">Like: <?= $search['a.pname'] ?></div>
-                <?php endif; ?>
-                <?= isset($search['active']) ? '<a data-search_reset href="javascript:void(0)" title="Reset search" class="reset"><i class="fa fa-2x fa-times" aria-hidden="true"></i></a>' : '' ?>
-              </div>
-              <b class="sr-ds">
-                <i class="fa fa-2x fa-chevron-right"></i>
-              </b>
+    <div class="panel panel-default search-panel">
+      <div class="panel-heading one-field">
+        <div class="row">
+          <div class="col-xs-10 col-sm-11">
+            <div class="form-row">
+              <input type="text" class="input-text" placeholder="Like ..." name="search[a.pname]"
+                     value="<?= isset($search['a.pname']) ? $search['a.pname'] : '' ?>">
             </div>
           </div>
-        </div>
-
-        <div class="panel-body hidden">
-
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="form-row">
-                <label>Product/Type/Color/Pattern/Manufacturer Name or Product Number:</label>
-                <input type="text" class="input-text" placeholder="Like ..." name="search[a.pname]"
-                       value="<?= isset($search['a.pname']) ? $search['a.pname'] : '' ?>">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="panel-footer hidden">
-          <div class="row">
-            <div class="col-xs-12">
-              <a data-search_submit class="button pull-right" href="<?= $action ?>">Search</a>
-            </div>
+          <div class="col-xs-2 col-sm-1">
+            <?= isset($search['active']) ? '<a data-search_reset href="javascript:void(0)" title="Reset search" class="search-reset"><i class="fa fa-2x fa-times" 
+aria-hidden="true"></i></a>' : '' ?>
+            <a data-search_submit class="pull-right search-button" href="<?= $action ?>">
+              <i class="fa fa-3x fa-search"></i>
+            </a>
           </div>
         </div>
       </div>
     </div>
-
   <?php endif; ?>
   <?php
   if(isset($search['hidden'])):
