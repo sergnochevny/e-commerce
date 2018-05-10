3d4fdb1e9d2c65d3d5be07ad49e20abc4a8fecbc
Serg Nochevny <serg.nochevny@engineering.ait.com>
2018-05-10 15:27:06 +0300

    the mail forms fix.

--- views/mail/welcome-mail-text.php
+++ views/mail/welcome-mail-text.php
@@ -8,8 +8,10 @@
  */
 
 $body = "Thank you for registering with iluvfabrix.com.\n\n";
-$body .= "As a new user, you will get 20% off your first purchase (which you may use any time in the first year) unless we have a sale going on for a discount greater than 20%, in which case you get the greater of the two discounts.\n\n";
-$body .= "We will, from time to time, inform you by email of various time limited specials on the iluvfabrix site.  If you wish not to receive these emails, please respond to this email with the word Unsubscribe in the subject line.\n\n";
+$body .= "As a new user, you will get 20% off your first purchase (which you may use any time in the first year) unless we have a sale going on for a discount greater than 20%, in which case you get the greater of the two discounts.\n";
+$body .= "We will, from time to time, inform you by email of various time limited specials on the iluvfabrix site.  If you wish not to receive these emails, please respond to this email with the word Unsubscribe in the subject line.\n";
 $body .= "Once again, thank you, and enjoy shopping for World Class Designer Fabrics & Trims on iluvfabrix.com.\n\n";
+$body .= "Sincerely,\n";
+$body .= $site_name . " Team!\n";
 
 echo $body;
